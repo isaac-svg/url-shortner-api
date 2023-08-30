eac9f75126baae34c473ce40f35261226b935cd4
isaac-svg <isaacsakyi009@gmail.com>
2023-08-30 20:41:51 +0000

    url-shortner-api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 dotenv.config()
+import cors from "cors"
 import  express  from 'express';
 import startDB from './DB/connection.js';
 import  createRoute from "./routes/gen-url.js"
@@ -7,8 +8,8 @@ const app = express();
 const port = 3000;
 
 
+app.use(cors({origin:"*"}))
 app.use(express.json())
-
 app.use("/",createRoute)
 app.listen(port, () => {
   startDB()
